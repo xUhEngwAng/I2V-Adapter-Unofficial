97004116ebf7b57e3d124d969f7b24122dd659bb
xUhEngwAng <1551885@tongji.edu.cn>
2024-03-11 10:47:34 +0800

    add a unittest to test whether different checkpoints of I2VAdapterModule have the same parameters.

--- test/test_i2v_adapter.py
+++ test/test_i2v_adapter.py
@@ -4,6 +4,7 @@ import unittest
 
 sys.path.append('./')
 
+from src.modules.i2v_adapter import I2VAdapterModule
 from src.modules.i2v_adapter import I2VAdapterTransformerBlock
 from src.modules.i2v_adapter import I2VAdapterTransformer2DModel
 
@@ -15,7 +16,7 @@ class TestI2VAdapterTransformer2DModel(unittest.TestCase):
         self.num_layers = 1
         self.cross_attention_dim = 2 * self.out_channels
         self.norm_num_groups = 32
-        
+
         self.i2v_adapter_transformer_2d_model = I2VAdapterTransformer2DModel(
             self.num_attention_heads,
             self.out_channels // self.num_attention_heads,
@@ -36,7 +37,7 @@ class TestI2VAdapterTransformer2DModel(unittest.TestCase):
 
         hidden_states = torch.randn((batch_frames, self.in_channels, width, height))
         encoder_hidden_states = torch.randn((batch_frames , encoder_seq_len, self.cross_attention_dim))
-        
+
         output = self.i2v_adapter_transformer_2d_model(
             hidden_states,
             enable_cross_frame_attn=True,
@@ -46,7 +47,7 @@ class TestI2VAdapterTransformer2DModel(unittest.TestCase):
             encoder_attention_mask=None,
             return_dict=False
         )[0]
-        
+
         self.assertEqual(output.shape, (batch_frames, self.out_channels, width, height))
 
     def test_I2VAdapterTransformer2DModel_no_cross_frame_output_shape(self):
@@ -57,7 +58,7 @@ class TestI2VAdapterTransformer2DModel(unittest.TestCase):
 
         hidden_states = torch.randn((batch_size, self.in_channels, width, height))
         encoder_hidden_states = torch.randn((batch_size , encoder_seq_len, self.cross_attention_dim))
-        
+
         output = self.i2v_adapter_transformer_2d_model(
             hidden_states,
             enable_cross_frame_attn=False,
@@ -66,7 +67,7 @@ class TestI2VAdapterTransformer2DModel(unittest.TestCase):
             encoder_attention_mask=None,
             return_dict=False
         )[0]
-        
+
         self.assertEqual(output.shape, (batch_size, self.out_channels, width, height))
 
 class TestI2VAdapterTransformerBlock(unittest.TestCase):
@@ -80,7 +81,7 @@ class TestI2VAdapterTransformerBlock(unittest.TestCase):
         self.dropout = 0.0
         self.cross_attention_dim = 2 * self.hidden_size
         self.activation_fn = 'gelu'
-       
+
         self.i2v_adapter_transformer_block = I2VAdapterTransformerBlock(
             self.hidden_size,
             self.num_attention_heads,
@@ -122,5 +123,30 @@ class TestI2VAdapterTransformerBlock(unittest.TestCase):
 
         self.assertEqual(output.shape, (self.batch_size, self.seq_len, self.hidden_size))
 
+def have_same_parameters(layers, param_name):
+    params = [layer.state_dict().get(param_name) for layer in layers]
+    res = torch.ones(params[0].shape, dtype=torch.bool)
+
+    for ind in range(len(layers)-1):
+        res = torch.logical_and(res, torch.eq(params[ind], params[ind+1]))
+
+    return torch.all(res)
+
+class testI2VAdapterModule(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_I2VAdapterModule_checkpoint(self):
+        epochs = [10, 20, 30]
+        checkpoint_paths = [f'./checkpoint/I2VAdapter-sample-5000/epoch_{n_epoch}' for n_epoch in epochs]
+        i2v_adapter_modules = [I2VAdapterModule.from_pretrained(path) for path in checkpoint_paths]
+        keys = i2v_adapter_modules[0].state_dict().keys()
+
+        for param_name in keys:
+            if have_same_parameters(i2v_adapter_modules, param_name):
+                print(f'Same parameters for {param_name}.')
+
+
 if  __name__ == "__main__":
     unittest.main()
+
